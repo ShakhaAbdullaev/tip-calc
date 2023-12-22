97992381192e381a1374820c024e086746cde088
ShakhaAbdullaev <shaxapilot@gamil.com>
2023-12-22 15:19:32 +0500

    done css

---
+++ script.js
@@ -0,0 +1,5 @@
+let input = document.getElementsByClassName('.bill_input');
+let tip = document.getElementsByClassName('.tip_percentage_input');
+let calculate = document.getElementsByClassName('.calc_btn');
+let result = document.getElementsByClassName('.calc_amount');
+
